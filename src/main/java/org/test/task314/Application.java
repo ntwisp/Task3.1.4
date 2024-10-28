cf703d76b8e9c1a6beeaa615c74a9fe5e5a9b94f
ntwisp <liangush@yandex.ru>
2024-10-28 19:23:10 +0300

    Task completed

--- src/main/java/org/test/task314/Application.java
+++ src/main/java/org/test/task314/Application.java
@@ -1,13 +1,21 @@
 package org.test.task314;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.test.task314.model.User;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        Communication communication = new Communication();
+        List<User> allUsers = communication.getAllUsers();
+        StringBuilder code = new StringBuilder();
+        User newUser = new User(3L, "James", "Brown", (byte) 25);
+        code.append(communication.saveUser(newUser));
+        newUser.setName("Thomas");
+        newUser.setLastName("Shelby");
+        code.append(communication.updateUser(newUser));
+        code.append(communication.deleteUser(3L));
+        System.out.println("Code: " + code);
     }
-
 }
